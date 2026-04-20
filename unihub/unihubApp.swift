c499ee78aba8dee5dfeaacf9f577585ec584f359
lipe <somavieluis87@gmail.com>
2026-04-20 17:46:55 +0100

    Initial Commit

---
+++ unihub/unihubApp.swift
@@ -0,0 +1,17 @@
+//
+//  unihubApp.swift
+//  unihub
+//
+//  Created by Luis Somavie on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct unihubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
